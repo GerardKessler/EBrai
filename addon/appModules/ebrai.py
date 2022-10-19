1a19173d867681b0e46e9c985a462b3a3c76e1aa
matrix-arg <delsur@live.com.ar>
2022-10-18 22:06:46 -0300

    init

---
+++ addon/appModules/ebrai.py
@@ -0,0 +1,22 @@
+﻿# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Gerardo Kessler <ReaperYOtrasYerbas@gmail.com>
+# This file is covered by the GNU General Public License.
+
+import appModuleHandler
+from scriptHandler import script
+import api
+from re import search
+from ui import message
+
+class AppModule(appModuleHandler.AppModule):
+
+	@script(gesture="kb:control+shift+e")
+	def script_positionAnnounce(self, gesture):
+		obj = api.getForegroundObject().children[1].children[6].children[4].name
+		message(obj)
+
+	@script(gesture="kb:control+shift+c")
+	def script_columnAnnounce(self, gesture):
+		obj = api.getForegroundObject().children[1].children[6].children[4].name
+		columnas = search(r"Columna.{2,4}\d{1,2}", obj)
+		message(columnas[0])
---
+++ readme.md
@@ -0,0 +1,6 @@
+# EBrai
+
+Pequeñísimo módulo que agrega 2 atajos de teclado para obtener los datos de la barra de estado
+
+* control + shift + e; verbaliza la barra de estado completa
+* control + shift + c; verbaliza el número de columna
